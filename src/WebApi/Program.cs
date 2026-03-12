3740b0e643621d582665c7659cc272c86d44bac4
Michal Gajda <michal@gajda.co.uk>
2026-03-12 13:22:06 +0000

    Add Hyaena

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -2,15 +2,54 @@ namespace Crane.WebApi;
 
 using Crane.Application;
 using Crane.Infrastructure;
+using MassTransit.Logging;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
-public class Program
+public static class Program
 {
+    private const string SERVICE_NAME = "crane";
+    private const string SERVICE_NAMESPACE = "poc";
+    private const string SERVICE_VERSION = "1.0.0";
+
     public static async Task Main(string[] args)
     {
+        var resourceBuilder = ResourceBuilder
+            .CreateDefault()
+            .AddService(serviceName: SERVICE_NAME, serviceVersion: SERVICE_VERSION)
+            .AddAttributes(new KeyValuePair<string, object>[]
+            {
+                new("service.namespace", SERVICE_NAMESPACE),
+                new("service.instance.id", Environment.MachineName)
+            });
+
         var builder = WebApplication.CreateBuilder(args);
 
         builder.Services.AddApplication();
         builder.Services.AddInfrastructure(builder.Configuration);
+
+        builder.Logging.AddOpenTelemetry(options => options.SetResourceBuilder(resourceBuilder).AddOtlpExporter());
+
+        builder.Services.AddOpenTelemetry()
+            .WithTracing(tracing => tracing
+                .SetResourceBuilder(resourceBuilder)
+                .SetSampler(new AlwaysOnSampler())
+                .AddAspNetCoreInstrumentation(opt => opt.RecordException = true)
+                .AddHttpClientInstrumentation(opt => opt.RecordException = true)
+                .AddSource(DiagnosticHeaders.DefaultListenerName)
+                .AddOtlpExporter())
+            .WithMetrics(metrics => metrics
+                .SetResourceBuilder(resourceBuilder)
+                .AddProcessInstrumentation()
+                .AddRuntimeInstrumentation()
+                .AddAspNetCoreInstrumentation()
+                .AddHttpClientInstrumentation()
+                .AddOtlpExporter())
+                ;
+
+        builder.Services.AddHealthChecks();
 
         builder.Services.AddControllers();
         builder.Services.AddOpenApi();
--- src/WebUI/Program.cs
+++ src/WebUI/Program.cs
@@ -1,9 +1,9 @@
+namespace Crane.WebUI;
+
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace Crane.WebUI;
-
-public class Program
+public static class Program
 {
     public static async Task Main(string[] args)
     {
